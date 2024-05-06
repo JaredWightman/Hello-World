0030e985e1f219a1a687a0452f830b86d8ae778c
JaredWightman <168222832+JaredWightman@users.noreply.github.com>
2024-05-06 12:25:11 -0600

    Made rectangle function.

--- Hello World/Shapes.py
+++ Hello World/Shapes.py
@@ -1,6 +1,17 @@
 # This will calculate the shapes areas.
 
+import re
+
+
+def calculate_rectangle_area(width, height):
+    area = width * height
+    return area
+
 def main():
     print("This is our shapes program.")
     
+    rectangle_area = calculate_rectangle_area(10,20)
+    print(("The area is {}").format(rectangle_area))
+
+
 main()
