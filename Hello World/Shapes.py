d265b9f64f9e2a2a79dafe2e1655166079d4eb3b
JaredWightman <168222832+JaredWightman@users.noreply.github.com>
2024-05-08 12:04:26 -0600

    added triangle print statement

--- Hello World/Shapes.py
+++ Hello World/Shapes.py
@@ -6,11 +6,18 @@ def calculate_rectangle_area(width, height):
     area = width * height
     return area
 
+def calculate_triangle_area(base, height):
+    area = .5 * base * height
+    return area
+
 def main():
     print("This is our shapes program.")
     
     rectangle_area = calculate_rectangle_area(10,20)
     print(("The area is {}").format(rectangle_area))
+    
+    tri_area = calculate_triangle_area(10,20)
+    print(("The area is {}").format(tri_area))
 
 
 main()
